fb5e4c0c1fcfa95eccef900496931695911618a5
Taishi <taishi2002i@gmail.com>
2023-06-27 11:27:27 +0900

    整形

--- src/Seats.js
+++ src/Seats.js
@@ -14,82 +14,72 @@ class Seats extends React.Component {
 
   handleClick = (i) => {
     const seats = this.state.seats.slice();
-    if(seats[i]===0){
+    if (seats[i] === 0) {
       seats[i] = null;
     } else {
       seats[i] = 0;
     }
-    this.setState({seats: seats});
-  }
+    this.setState({ seats: seats });
+  };
 
   resultSeats = (num) => {
     const seats = this.state.seats.slice();
-    let indexSeats=0;
-    let indexNum=0;
-    while(indexNum<num.length) {
-      if (seats[indexSeats]!==0) {
+    let indexSeats = 0;
+    let indexNum = 0;
+    while (indexNum < num.length) {
+      if (seats[indexSeats] !== 0) {
         seats[indexSeats] = num[indexNum];
         indexNum++;
       }
       indexSeats++;
     }
-    this.setState({seats: seats});
-    this.setState({viewChange: false});
-  }
+    this.setState({ seats: seats, viewChange: false });
+  };
 
   renderSeats(i) {
-    return (
-    <SetSeats
-      value={i}
-      onClick={this.handleClick}
-      />
-    );
+    return <SetSeats value={i} onClick={this.handleClick} />;
   }
 
-  ViewSeats(w,h){
-    return(h.map((index) => (
+  ViewSeats(w, h) {
+    return h.map((index) => (
       <div className="board-row" key={index}>
-        {w.map((number) => (
-          this.renderSeats(number+7*index)
-        ))}
+        {w.map((number) => this.renderSeats(number + 7 * index))}
       </div>
-    )))
+    ));
   }
+
   renderResult(i) {
-    return (
-    <Result
-      value={this.state.seats[i]}
-      />
-    );
+    return <Result value={this.state.seats[i]} />;
   }
 
-  ViewResult(w,h){
-    return(h.map((index) => (
+  ViewResult(w, h) {
+    return h.map((index) => (
       <div className="board-row" key={index}>
-        {w.map((number) => (
-          this.renderResult(number+7*index)
-        ))}
+        {w.map((number) => this.renderResult(number + 7 * index))}
       </div>
-    )))
-  }
-
-  ViewController(w,h){
-    if(this.state.viewChange)return this.ViewSeats(w,h)
-    else return this.ViewResult(w,h)
+    ));
   }
 
   render() {
-    const widthNumber = [0,1,2,3,4,5,6];
-    const heigth = [0,1,2,3,4,5,6,7];
+    const widthNumber = [0, 1, 2, 3, 4, 5, 6];
+    const height = [0, 1, 2, 3, 4, 5, 6, 7];
     return (
       <div>
-        {this.ViewController(widthNumber,heigth)}
+        {this.state.viewChange ? (
+          this.ViewSeats(widthNumber, height)
+        ) : (
+          this.ViewResult(widthNumber, height)
+        )}
         <div className="viewChange">
-          <ChangeSeats
-          seats={this.state.seats}
-          onClick={this.resultSeats}
-          />
-          <button className='Button' onClick={() => {this.setState({viewChange:true,seats: Array(56).fill(null)})}}>再抽選</button>
+          <ChangeSeats seats={this.state.seats} onClick={this.resultSeats} />
+          <button
+            className="Button"
+            onClick={() => {
+              this.setState({ viewChange: true, seats: Array(56).fill(null) });
+            }}
+          >
+            再抽選
+          </button>
         </div>
       </div>
     );
