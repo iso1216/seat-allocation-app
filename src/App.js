500f3b44743c23e91e1b77acd706b50dacc35c96
Taishi <taishi2002i@gmail.com>
2023-10-04 12:05:05 +0900

    改修

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ export default function App(){
   const [value, setValue] = useState(0);
   const [heightNum, setHeight] = useState(1);
   const [widthNum, setWidth] = useState(1);
+  const [seats,setSeats] = useState(Array(150).fill(0));
   const ViewChange = (value) => {
     switch (value) {
       case 0:
         return <SelectValue setValue={setValue} setHeight={setHeight} setWidth={setWidth} />;
       case 1:
-        return <Seats setValue={setValue} heightNum={heightNum} widthNum={widthNum} />;
+        return <Seats setValue={setValue} heightNum={heightNum} widthNum={widthNum} seats={seats} setSeats={setSeats} />;
       case 2:
         return <Loading setValue={setValue} />;
       case 3:
--- src/components/Seats.js
+++ src/components/Seats.js
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 
-export default function Seats({setValue, heightNum, widthNum}) {
+export default function Seats({setValue, heightNum, widthNum, seats, setSeats}) {
   return(
     <Box></Box>
   );
