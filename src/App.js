82fea7dbcbb2fee03038926c54049c346c4ef172
Taishi <taishi2002i@gmail.com>
2023-10-04 11:59:11 +0900

    改修開始、 初期画面の作成

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Container } from "@mui/material";
+import { useState } from "react";
+import Seats from "./components/Seats";
+import Result from "./components/result";
+import SelectValue from "./components/SelectValue";
+import Loading from "./components/Loading";
+
+export default function App(){
+  const [value, setValue] = useState(0);
+  const [heightNum, setHeight] = useState(1);
+  const [widthNum, setWidth] = useState(1);
+  const ViewChange = (value) => {
+    switch (value) {
+      case 0:
+        return <SelectValue setValue={setValue} setHeight={setHeight} setWidth={setWidth} />;
+      case 1:
+        return <Seats setValue={setValue} heightNum={heightNum} widthNum={widthNum} />;
+      case 2:
+        return <Loading setValue={setValue} />;
+      case 3:
+        return <Result setValue={setValue} heightNum={heightNum} widthNum={widthNum} />;
+      default:
+        break;
+    }
+  }
+
+  return(
+    <Container>
+      {ViewChange(value)}
+    </Container>
+  );
+};
---
+++ src/components/Loading.js
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+export default function Loading(){
+  return(
+    <Box></Box>
+  );
+};
